b0af34a40c37e982a2a0afd29b79335c9a0527a0
davimen1017 <davi.menezes.silva@escola.pr.gov.br>
2025-05-20 11:32:28 -0300

    valores e tipos

---
+++ pythonProject/Capítulo 2/exemplo2valoresetipos.py
@@ -0,0 +1,16 @@
+#tipo inteiro: 1,2,3... -2,-3,-4
+inteiro = 2
+print(type(inteiro))
+
+#tipo real ou ponto flutuante, float 1.3,0.6,... -3.156...
+float = 1.3
+print(type(float))
+
+#caracteres
+char1="a"
+char2="2"
+print(type(char1))
+
+#string, conjunto de caracteres
+stringNome = "Davi Menezes"
+print(type(stringNome))
