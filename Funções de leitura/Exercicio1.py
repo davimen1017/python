aeb435808ed425a9965ac14939dd9194695119c3
maria <lopes.cruz.maria@escola.pr.gov.br>
2025-09-23 11:49:14 -0300

    Funções de leitrura

---
+++ Funções de leitura/Exercicio1.py
@@ -0,0 +1,9 @@
+def apresentacao (nome, nomeMae, nomePai):
+    print(f'Nome:{nome}')
+    print(f'Nome da mãe:{nomeMae} ')
+    print(f'Nome do pai:{nomePai}')
+nome = str(input('Digite o seu nome: '))
+nomePai = str(input('Digite o nome do seu pai: '))
+nomeMae = str(input('Digite o nome da sua mãe: '))
+
+apresentacao (nome, nomeMae,nomePai)
